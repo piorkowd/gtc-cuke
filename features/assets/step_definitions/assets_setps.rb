ed5890e7b6eb8295f09e0fb5d84f5d806c6c592d
Daniel Piorkowski <danpiorkowski@gmail.com>
2012-09-26 12:31:52 -0700

    Add a wait for loading

--- features/assets/step_definitions/assets_setps.rb
+++ features/assets/step_definitions/assets_setps.rb
@@ -2,6 +2,8 @@ mobileAssetsUrl = "https://qa1.zonarsystems.net/mobile/assets"
 
 When /^I click the "(.*)" link$/ do |linkName|
 	@browser.link(:text => "#{linkName}").click
+	@browser.html(:class => "ui-mobile ui-loading").wait_until_present
+	@browser.html(:class => "ui-mobile ui-loading").wait_while_present
 	
 	if ENV['CHROME']
 		add_runtime
