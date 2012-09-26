043abee4de1621818706f5a3a072de13f7b302b5
Daniel Piorkowski <daniel.piorkowski@parivedasolutions.com>
2012-09-26 15:56:08 -0700

    Implement mobile asset tests
    Added .DS_Store to .gitignore

--- features/assets/assets.feature
+++ features/assets/assets.feature
@@ -7,7 +7,7 @@ Feature: Assets
 	@positive @mobile
 	Scenario: View asset menu
 		Given I am logged into Zonar
-		When I click the "Asset Lookup" link
+		When I click the Asset Lookup link
 		Then I should be viewing the assets page
 	
 #	@positive @chrome
@@ -16,16 +16,16 @@ Feature: Assets
 #		When I click the "Admin" link
 #		And When I click the 
 
-#	@positive
-#	Scenario: Find an asset
-#		Given I am logged into Zonar
-#		And I am viewing the assets page
-#		When I search for GMC assets
-#		Then I should see a GMC asset
+	@positive
+	Scenario: Find an asset
+		Given I am logged into Zonar
+		And I am viewing the assets page
+		When I search for Ford assets
+		Then I should only see Ford assets
 		
-#	@fail
-#	Scenario: Expect an impossible result 
-#		Given I am logged into Zonar
-#		And I am viewing the assets page
-#		When I search for GMC assets
-#		Then I should see a Dodge asset
+	@fail
+	Scenario: Expect an impossible result 
+		Given I am logged into Zonar
+		And I am viewing the assets page
+		When I search for GMC assets
+		Then I should only see Dodge assets
--- features/assets/step_definitions/assets_steps.rb
+++ features/assets/step_definitions/assets_steps.rb
@@ -1,18 +1,50 @@
 mobileAssetsUrl = "https://qa1.zonarsystems.net/mobile/assets"
+mobileUrl = "https://qa1.zonarsystems.net/mobile"
 
-When /^I click the "(.*)" link$/ do |linkName|
-	@browser.link(:text => "#{linkName}").click
+Given /^I am viewing the assets page$/ do
+	if ENV['IPHONE']
+		if (@browser.url != mobileAssetsUrl) then
+			@browser.goto mobileUrl
+			@browser.link(:text => "Asset Lookup").click
+			@browser.ul(:class => "ui-listview").li(:class => "btn_as ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-hover-d ui-btn-up-d ui-btn-active").wait_while_present
+		end
+	end
+end
+
+When /^I click the Asset Lookup link$/ do
+	# Mobile only
+	@browser.link(:text => "Asset Lookup").click
 	@browser.ul(:class => "ui-listview").li(:class => "btn_as ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-hover-d ui-btn-up-d ui-btn-active").wait_while_present
-	
-	if ENV['CHROME']
-		add_runtime
+end
+
+When /^I search for (.*) assets$/ do |searchTerm|
+	if ENV['IPHONE']
+		@browser.text_field(:class => "ui-input-text ui-body-c").set "#{searchTerm}"
 	end
 end
 
 Then /^I should be viewing the assets page$/ do
-	if (@browser.url != mobileAssetsUrl) then
-		puts "Expected final URL: #{mobileAssetsUrl}"
-		puts "Actual final URL: #{@browser.url}"
-		raise
+	if ENV['IPHONE']
+		if (@browser.url != mobileAssetsUrl) then
+			puts "Expected final URL: #{mobileAssetsUrl}"
+			puts "Actual final URL: #{@browser.url}"
+			raise
+		end
+	end
+end
+
+Then /^I should only see (.*) assets$/ do |assetMatch|
+	if ENV['IPHONE']
+		listItems = @browser.ul(:class => "ui-listview ui-listview-inset ui-corner-all ui-shadow").lis
+		
+		listItems.each do |li|
+			if (!li.attribute_value(:class).include? "ui-screen-hidden")
+				if (!li.text.downcase.include? "#{assetMatch.downcase}")
+					puts "Expected to see in asset text: #{assetMatch}"
+					puts "Actual asset text: #{li.text}"
+					raise
+				end
+			end
+		end
 	end
 end
